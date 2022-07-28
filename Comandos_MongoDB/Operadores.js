16f5725b2a5aa57666b77f4383ce7c3d7d5b1f3d
alexrocha97 <venturarocha08@gmail.com>
2022-07-28 18:20:43 -0300

    Mais conhecimento sobre mongoDB

--- Comandos_MongoDB/Operadores.js
+++ Comandos_MongoDB/Operadores.js
@@ -60,3 +60,50 @@ db.books.find({
 // Contar o números de resultado
 // sintaxe:
 db.books.find({pageCount: 600}).count()
+
+
+// Update One 
+db.books.updateOnde({_id: 314}, { $set: {pageCount: 1000}})
+// Exemplo de um atualização e sua sintaxe
+// O operador $set é onde fica os valores que serão atualizados
+
+// Update Many
+db.books.updateMany(
+    {categories: "JAVA"}, 
+    { $set: {status: "UNPUBLISHED"}
+})
+// Update Many com adição de uma chave e um valor
+db.books.updateMany(
+    {authors: "Vikram Goyal"},
+    { $set: {download: 1000}
+})
+// Fazendo um update em arquivos com filtro
+db.books.updateMany(
+    {pageCount: { $gt: 500}}
+    ,{ $set: {bestseller: true}
+})
+// ReplaceOne
+// essa sintaxe vai substituir os arquivos salvo nesse id e colocar o novo document
+db.books.replaceOne(
+    {_id: 120},
+    {foi: "Subistiuido"}
+);
+// Adicionando um item em um array
+// Nessa sintaxe passamos o id do documento, depois abrimos outro documento
+// usamos o operador $push e colocamos a chave do array e o valor que queremos
+// adicionar
+db.books.updateOne(
+    {_id: 201},
+    { $push: {categories: "PHP"}
+})
+// Para atualizar todos os registros do banco
+db.books.updateMany(
+    {},
+    { $set: {atualizando: true}
+})
+// Nesse caso eu estou adicionando em todos document uma chave e valor nova
+
+
+// DELETE
+// Sintaxe
+db.books.deleteOne({_id: 20});
