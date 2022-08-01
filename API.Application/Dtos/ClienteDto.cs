6d1babbd270c2c07d9b33aeca3aa77a3ff7b312e
alexrocha97 <venturarocha08@gmail.com>
2022-08-01 00:12:56 -0300

    construtor do Dto

--- API.Application/Dtos/ClienteDto.cs
+++ API.Application/Dtos/ClienteDto.cs
@@ -2,6 +2,14 @@ namespace API.Application.Dtos
 {
     public class ClienteDto
     {
+        public ClienteDto(int? id, string nome, string sobrenome, string email)
+        {
+            Id = id;
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.email = email;
+        }
+
         public int? Id {get; set;}
         public string nome {get; set;}
         public string sobrenome {get; set;}
--- API.Application/Dtos/ProdutoDto.cs
+++ API.Application/Dtos/ProdutoDto.cs
@@ -2,6 +2,13 @@ namespace API.Application.Dtos
 {
     public class ProdutoDto
     {
+        public ProdutoDto(int? id, string nome, decimal valor)
+        {
+            Id = id;
+            this.nome = nome;
+            this.valor = valor;
+        }
+
         public int? Id {get; set;}
         public string nome {get; set;}
         public decimal valor {get; set;}
